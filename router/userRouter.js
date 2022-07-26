5dbcac8ede0abf25f4c4d48447def9243d5b2aa5
hasibulislam999 <hasib143sl@gmail.com>
2022-07-26 22:23:08 +0600

    integrating add, get and remove user within route

--- router/userRouter.js
+++ router/userRouter.js
@@ -2,11 +2,32 @@
 const express = require('express');
 
 // internal imports
-const { getUsers } = require('../controller/userController');
+const {
+    getUsers,
+    addUser,
+    removeUser
+} = require('../controller/userController');
 const decorateHTMLResponse = require('../middleware/common/decorateHTMLResponse');
+const avatarUpload = require('../middleware/users/avatarUpload');
+const {
+    addUserValidator,
+    addUserValidationHandler
+} = require('../middleware/users/userValidators');
 
 const router = express.Router();
 
+// user page
 router.get("/", decorateHTMLResponse("User"), getUsers);
+
+// add user
+router.post("/",
+    avatarUpload,
+    addUserValidator,
+    addUserValidationHandler,
+    addUser
+);
+
+// remove user
+router.delete("/:id", removeUser);
 
 module.exports = router;
