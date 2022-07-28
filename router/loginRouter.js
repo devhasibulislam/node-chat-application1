a046a924002753a376c976152317082291642d8c
hasibulislam999 <hasib143sl@gmail.com>
2022-07-28 23:40:18 +0600

    integrating auth gurd middleware in login router

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -1,12 +1,33 @@
 // external imports
-const express = require('express');
+const express = require("express");
 
 // internal imports
-const { getLogin } = require('../controller/loginController');
-const decorateHTMLResponse = require('../middleware/common/decorateHTMLResponse');
+const { getLogin, login, logout } = require("../controller/loginController");
+const decorateHtmlResponse = require("../middleware/common/decorateHtmlResponse");
+const {
+    doLoginValidators,
+    doLoginValidationHandler,
+} = require("../middleware/loginValidators");
+const { redirectLoggedIn } = require("../middleware/common/checkLogin");
 
 const router = express.Router();
 
-router.get("/", decorateHTMLResponse("Login"), getLogin);
+// set page title
+const page_title = "Login";
+
+// login page
+router.get("/", decorateHtmlResponse(page_title), redirectLoggedIn, getLogin);
+
+// process login
+router.post(
+    "/",
+    decorateHtmlResponse(page_title),
+    doLoginValidators,
+    doLoginValidationHandler,
+    login
+);
+
+// logout
+router.delete("/", logout);
 
 module.exports = router;
