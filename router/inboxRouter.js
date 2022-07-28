35a656a09c5160b0156f090000bbfb8b9df0b87d
hasibulislam999 <hasib143sl@gmail.com>
2022-07-28 23:40:08 +0600

    integrating auth gurd middleware in inbox router

--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -4,9 +4,10 @@ const express = require('express');
 // internal imports
 const { getInbox } = require('../controller/inboxController');
 const decorateHTMLResponse = require('../middleware/common/decorateHTMLResponse');
+const { checkLogin } = require("../middleware/common/checkLogin");
 
 const router = express.Router();
 
-router.get("/", decorateHTMLResponse("Inbox"), getInbox);
+router.get("/", decorateHTMLResponse("Inbox"), checkLogin, getInbox);
 
 module.exports = router;
